7f611d6a9bf98b49b4d646df801fa104d4c465aa
Dave Roberts <d.roberts@ctrlo.com>
2024-01-25 15:56:21 +0000

    Initial setup - still needs slave/master

--- Common/PowerDNS.pm
+++ Common/PowerDNS.pm
@@ -2,13 +2,6 @@ package Common::PowerDNS;
 
 use Rex -base;
 
-use Data::Dumper;
-
-task get_ips => sub {
-    my $net_info = run 'ip addr show ens3';
-    set connection->server, $net_info =~ 'inet\s(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})';
-};
-
 task install_packages => sub {
     update_package_db;
     pkg [qw/pdns-server pdns-backend-mysql mariadb-server mariadb-client/], ensure => "present";
@@ -46,26 +39,6 @@ task setup_mysql_connection => sub {
         line => "gmysql-password=5ecur3Pa55w0rd",
         regexp => '^gmysql-password=YOUR_PASSWORD_HERE',
         on_change => sub { service "pdns" => "restart"; };
-};
-
-task "set_master", sub {
-    append_or_amend_line '/etc/powerdns/pdns.conf',
-        line => "master=yes",
-        regexp => '^(# )?master=no',
-        on_change => sub { service "pdns" => "restart"; };
-};
-
-task "set_slave", sub {
-    append_or_amend_line '/etc/powerdns/pdns.conf',
-        line => "slave=yes",
-        regexp => '^(# )?slave=no';
-    append_or_amend_line '/etc/powerdns/pdns.conf',
-        line => "slave-cycle-interval=60",
-        regexp => '^(# )?slave-cycle-interval=60';
-    my $nameserver = get("nameserver1",'');
-    die ("Invalid nameserver setting") if $nameserver eq '';
-    run "mysql pda -e < 'INSERT INTO supermasters VALUES (" . $nameserver . ", \'ns2.replecateme.dev\',\'admin\')'";
-    service "pdns" => "restart";
 };
 
 1;
--- Rexfile
+++ Rexfile
@@ -1,6 +1,7 @@
 use lib './Common';
 
 use Rex -base;
+use Rex::Commands::Host;
 
 include qw/
     Common::PowerDNS
@@ -13,37 +14,10 @@ key_auth;
 
 group nameservers => "nameserver1", "nameserver2";
 
-task "get_ips", group => "nameservers", sub {
-    Common::PowerDNS::get_ips();
-};
-
-task "pdns", group => "nameservers", sub {
+task "pdns", sub {
     Common::PowerDNS::install_packages();
     Common::PowerDNS::create_mariadb_user();
     Common::PowerDNS::create_mariadb_tables();
     Common::PowerDNS::disable_resolved();
     Common::PowerDNS::setup_mysql_connection();
 };
-
-task "set_master", "nameserver1", sub {
-    Common::PowerDNS::set_master();
-};
-
-task "set_slave", "nameserver2", sub {
-    Common::PowerDNS::set_slave();
-};
-
-task "init", "nameserver1", sub {
-    my $return = run 'pdnsutil create-zone replicated.dev';
-    Rex::Logger::info($return);
-    $return = run 'pdnsutil add-record replicated.dev @ NS nameserver1';
-    Rex::Logger::info($return);
-    $return = run 'pdnsutil add-record replicated.dev @ NS nameserver2';
-    Rex::Logger::info($return);
-    $return = run 'pdnsutil increase-serial replicated.dev';
-    Rex::Logger::info($return);
-    $return = run 'pdnsutil notify replicated.dev';
-    Rex::Logger::info($return);
-};
-
-batch "get_ips", "full_install", "pdns", "set_master", "set_slave";
